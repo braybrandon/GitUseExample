add5b4b7de35e7ae86c709de2d59c25914c09a7c
Brandon Bray <braybrandon59@outlook.com>
2020-08-20 13:23:44 -0400

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Brandon Bray on 8/20/20.
+//  Copyright © 2020 Brandon Bray. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
